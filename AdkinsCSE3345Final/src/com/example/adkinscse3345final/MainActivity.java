c890edeab4d253db6669dcf7bda02cb5769b63bd
jacobpadkins <jpadkins@smu.edu>
2014-12-12 03:17:17 -0600

    nothing working 100% yet. The app will create a slideshow using the Reddit API

--- AdkinsCSE3345Final/src/com/example/adkinscse3345final/MainActivity.java
+++ AdkinsCSE3345Final/src/com/example/adkinscse3345final/MainActivity.java
@@ -1,31 +1,153 @@
 package com.example.adkinscse3345final;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	// et is the EditText in MainActivity
-	EditText et;
+	private EditText et;
+	// bt is the button which creates a SlideShow intent
+	private Button bt;
+	// images is a List containing the urls of the images
+	private List<String> images = new ArrayList<String>();
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		// set handlers
+		et = (EditText) findViewById(R.id.editText);
+		bt = (Button) 	findViewById(R.id.button);
 		
-		String string = "www.reddit.com/r/funny/hot.json?sort=hot";
+		// set on click listener for button
+		bt.setOnClickListener(this);
 	}
 	
+	// function for Button bt's onClickListener
+	public void onClick(View v) {
+		if (v.getId() == R.id.button) {
+			// create callAPI object for networking
+			callAPI a = new callAPI();
+			// check if input is valid
+			try {
+				a.execute().get();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+			et.setText("");
+			// prepare to go to Slide Show
+			String[] imagesArray = images.toArray(new String[images.size()]);
+			Intent intent = new Intent(this, SlideShow.class);
+			intent.putExtra("images", imagesArray);
+			// call the intent
+			startActivity(intent);
+		}
+	}
+	
+	// returns a JSON string from a given url
+	public String getStringFromUrl(String url) {
+		String string;
+		StringBuilder builder = new StringBuilder();
+		HttpClient client = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(url);
+		try {
+			HttpResponse response = client.execute(httpGet);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+			if (statusCode == 200) {
+				HttpEntity entity = response.getEntity();
+				InputStream stream = entity.getContent();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+				while ((string = reader.readLine()) != null) {
+					builder.append(string);
+				}
+			}
+			else {
+				Log.e("API", "error reading from file.");
+			}
+		}
+		catch (ClientProtocolException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return builder.toString();
+	}
+	
+	// calls getStringFromUrl based on contents of et and parses
+	private void requestAndParse() {
+		String string = "http://www.reddit.com/r/" + et.getText().toString() + "/hot.json?sort=hot";
+		// replace spaces with + for the HttpGet object
+		string = string.replace(' ', '+');
+		string = getStringFromUrl(string);
+		// this is the default json response if the parameters were not correct
+		if (string == "{\"kind\": \"Listing\", \"data\": {\"modhash\": \"\", \"children\": [], \"after\": null, \"before\": null}}" 
+				|| string == "{\"error\": 404}") {
+			Toast toast = Toast.makeText(getApplicationContext(), "Invalid Subreddit Name!", Toast.LENGTH_SHORT);
+			toast.show();
+		}
+		// else the parameters were correct and we have a good json response
+		else {
+			images.clear();
+			Log.i("MainActivity", string);
+			try {
+				JSONArray jArray = new JSONObject(string).getJSONObject("data").getJSONArray("children");
+				JSONObject jObject;
+				// parse the individual image urls out of the JSONObject
+				for (int i = 0; i < jArray.length(); i++) {
+					jObject = jArray.getJSONObject(i).getJSONObject("data");
+					string = jObject.optString("url");
+					// check if it is a valid image url
+					if (string.endsWith(".jpg") || string.endsWith(".png")) {
+							images.add(jObject.optString("url"));
+					}
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
 	// callAPI extends AsyncTask for performing networking off main thread
 	final class callAPI extends AsyncTask<Void, Void, Void> {
+		
 		@Override
 		protected Void doInBackground(Void... params) {
-		return null;
-		
+			requestAndParse();
+			return null;
 		}
 	}
 }
--- AdkinsCSE3345Final/src/com/example/adkinscse3345final/SlideShow.java
+++ AdkinsCSE3345Final/src/com/example/adkinscse3345final/SlideShow.java
@@ -1,34 +1,83 @@
 package com.example.adkinscse3345final;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.ImageView;
 
 public class SlideShow extends Activity {
-
+	
+	// this String array contains the urls of the images
+	private String[] images;
+	// this is the handle for the ImageView
+	private ImageView imageView;
+	// this keeps track of the location in the images array;
+	private int location;
+	// this is an object of a class which extends AsyncTask
+	private cycleImages c;
+	// this is a handler for use in perforing actions at time intervals
+	private Handler handle;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_slide_show);
+		
+		// set variables
+		location = 0;
+		c = new cycleImages();
+		handle = new Handler();
+				
+		// set handlers
+		imageView = (ImageView) findViewById(R.id.image);
+		
+		// set images from the extra data sent by MainActivity
+		images = getIntent().getStringArrayExtra("images");
+		
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.slide_show, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	
+	// this function changes the image to the next one in the array
+	private void setImage() {
+		// if there are no more images, reset location and start over
+		if (location >= images.length) {
+			location = 0;
 		}
-		return super.onOptionsItemSelected(item);
+		// set the imageview by the urls in images[]
+		try {
+			final Bitmap bitmap = BitmapFactory.decodeStream((InputStream) new URL(images[location]).getContent());
+			Log.i("SlideShow", images[location]);
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					imageView.setImageBitmap(bitmap);
+				}
+			});
+			imageView.setImageBitmap(bitmap);
+			location++;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// cycleImages extends AsyncTask and is responsible for setting the images
+	final class cycleImages extends AsyncTask<Void, Void, Void> {
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+			setImage();
+			return null;
+		}
 	}
 }
